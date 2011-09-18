f5e99cef66200fc2bef31a1d682a82491499bfba
Nicolae Claudius <nicolae_claudius@yahoo.com>
2011-09-18 04:34:01 -0700

    smtp host format

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Simpledns::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'http://simpledns.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'simpledns.heroku.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
