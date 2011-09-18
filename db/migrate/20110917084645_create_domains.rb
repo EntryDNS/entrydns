ad643f7590d3887be9aef1af720a330e5983d650
Nicolae Claudius <nicolae_claudius@yahoo.com>
2011-09-18 10:16:08 -0700

    schema compliance

--- db/migrate/20110917084645_create_domains.rb
+++ db/migrate/20110917084645_create_domains.rb
@@ -3,7 +3,7 @@ class CreateDomains < ActiveRecord::Migration
     create_table :domains do |t|
       t.string :name, :limit => 255, :null => false
       t.string :master, :limit => 128
-      t.integer :last_check, :limit => 50, :null => false
+      t.integer :last_check, :limit => 50
       t.string :type, :limit => 6, :null => false
       t.integer :notified_serial
       t.string :account, :limit => 40
