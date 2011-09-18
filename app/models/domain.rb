1051ee0767ff71007f22117eeb95e65a9c880b3b
Nicolae Claudius <nicolae_claudius@yahoo.com>
2011-09-18 05:16:39 -0700

    sti & typo fix

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,3 +1,4 @@
 class Domain < ActiveRecord::Base
+  set_inheritance_column { 'sti_type' }
   has_many :records
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,4 @@
 class Record < ActiveRecord::Base
+  set_inheritance_column { 'sti_type' }
   belongs_to :domain
 end
