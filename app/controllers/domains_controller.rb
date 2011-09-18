586c1757221860ce2fa7362af657b578e70dca5b
Nicolae Claudius <nicolae_claudius@yahoo.com>
2011-09-18 11:19:13 -0700

    forms, default sorting

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,7 +1,10 @@
 class DomainsController < ApplicationController
   active_scaffold :domain do |conf|
     conf.columns = [:name, :records]
+    conf.create.columns = [:name]
+    conf.update.columns = [:name]
     conf.actions.exclude :show
+    conf.list.sorting = { :name => :asc }
   end
   
   protected
