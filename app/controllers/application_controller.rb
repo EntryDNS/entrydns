7db19de3665d9f0c60e1dd1d30a58b6ea0692fc1
Nicolae Claudius <nicolae_claudius@yahoo.com>
2011-09-21 07:02:20 -0700

    error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   include SentientController
   protect_from_forgery
+  
+  rescue_from CanCan::AccessDenied, ActiveScaffold::ActionNotAllowed do |exception|
+    flash.now[:error] = exception.message
+    render :template => 'errors/access_denied', :layout => 'errors'
+  end
+
+  rescue_from ActiveScaffold::ActionNotAllowed do |exception|
+    flash.now[:error] = I18n.t("action_not_allowed")
+    render :template => 'errors/access_denied', :layout => 'errors'
+  end
+  
 end
