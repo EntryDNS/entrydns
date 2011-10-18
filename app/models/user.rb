13a2db04c5f6ba91a64aa17151a67ff55cf97d4d
Nicolae Claudius <nicolae_claudius@yahoo.com>
2011-10-18 11:42:32 -0700

    account cancelation, article styles on app

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
   
-  has_many :records
+  has_many :domains, :inverse_of => :user, :dependent => :destroy
+  has_many :records, :inverse_of => :user, :dependent => :destroy
   
   def name
     first_name + ' ' + last_name
