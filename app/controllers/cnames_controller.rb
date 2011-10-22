46da0d9fdc3bfa2e32933453005b78cd1a841d6f
Nicolae Claudius <nicolae_claudius@yahoo.com>
2011-10-22 10:44:11 -0700

    empty name default

--- app/controllers/cnames_controller.rb
+++ app/controllers/cnames_controller.rb
@@ -26,7 +26,6 @@ class CnamesController < ApplicationController
   # override, we make our own sti logic
   def new_model
     model = beginning_of_chain.new
-    model.name = nested_parent_record.name
     model
   end
 
