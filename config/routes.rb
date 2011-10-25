a695fc71fcfe8de4329e2a583596c1cdbf0b6f03
Nicolae Claudius <nicolae_claudius@yahoo.com>
2011-10-25 12:26:50 -0700

    fix logout issue for some users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Entrydns::Application.routes.draw do
   
   devise_for :users
+  devise_scope :user do
+    get 'users/sign_out', :to => "devise/sessions#destroy"
+  end
   
   resources :domains do
     as_routes
