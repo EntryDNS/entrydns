5a6ec17e401b275dc18c610ba9d41d6446776785
Nicolae Claudius <nicolae_claudius@yahoo.com>
2011-12-30 05:46:15 -0800

    permission spec

--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Permission do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include_context "data"
+  
+  it "validates selfness" do
+    permission.user_id = permission.domain.user_id
+    permission.should have(1).error_on(:user)
+  end
+  
+  it "validates unexsisting email" do
+    permission.user_email = "does.not@exist.in.db"
+    permission.should have(1).error_on(:user)
+  end
 end
