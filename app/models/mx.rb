9f3818508a9b3b1c4d9833e9775cfef3ad625425
Nicolae Claudius <nicolae_claudius@yahoo.com>
2012-06-16 14:31:21 +0300

    support wildcard MX records, close #104

--- app/models/mx.rb
+++ app/models/mx.rb
@@ -7,8 +7,14 @@
 # Obtained from http://www.zytrax.com/books/dns/ch8/mx.html
 #
 class MX < Record
-  validates :name, :hostname => {:allow_underscore => true, :allow_wildcard_hostname => true}
-  validates :content, :presence => true, :hostname => true
+  validates :name, :hostname => {
+    :allow_underscore => true,
+    :allow_wildcard_hostname => true
+  }
+  validates :content, :presence => true, :hostname => {
+    :allow_underscore => true,
+    :allow_wildcard_hostname => true
+  }
   validates :prio, :presence => true, :numericality => {
     :greater_than_or_equal_to => 0, 
     :less_than_or_equal_to => 65535, 
