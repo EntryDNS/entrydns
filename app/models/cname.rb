e2acfe8d482f9e595dbf5937d20fc04b53c670c6
Claudius Nicolae <clyfe@n50.(none)>
2013-03-15 00:28:44 +0200

    oops

--- app/models/cname.rb
+++ app/models/cname.rb
@@ -8,10 +8,8 @@
 # Obtained from http://www.zytrax.com/books/dns/ch8/cname.html
 #
 class CNAME < Record
-  validates :name,
-    :length => { :maximum => 1024 }, 
-    :hostname2 =>  { :allow_underscore => true, :allow_wildcard_hostname => true }
-  validates :content, :presence => true, :hostname => true
+  validates :name, :hostname =>  {:allow_underscore => true, :allow_wildcard_hostname => true}
+  validates :content, :presence => true, :length => { :maximum => 1024 }, :hostname2 => true
 
 end
 
