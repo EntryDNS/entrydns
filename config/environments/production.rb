1128a4738f674291e934c3a4550fb5f3ac31df1f
Nicolae Claudius <claudius.nicolae@gmail.com>
2013-09-01 22:35:34 +0300

    no more support for old ie

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,6 @@ Entrydns::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( application-ie.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
