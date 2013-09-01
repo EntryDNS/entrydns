624ff3b3287cae681fdd866c55928dccfca52c6f
Nicolae Claudius <claudius.nicolae@gmail.com>
2013-09-01 23:12:30 +0300

    audited garbage

--- db/seeds/development/domains.seeds.rb
+++ db/seeds/development/domains.seeds.rb
@@ -2,17 +2,15 @@ after 'development:users' do
   
   entrydns_org = Domain.find_by_name(Settings.host_domains.first)
   User.all.each do |user|
-    Audit.as_user(user) do
-      20.times do
-        domain = Factory.build(:domain, :user => user)
-        domain.setup(FactoryGirl.generate(:email))
-        domain.save!
-        domain.soa_record.update_serial!
-      end
+    20.times do
+      domain = Factory.build(:domain, :user => user)
+      domain.setup(FactoryGirl.generate(:email))
+      domain.save!
+      domain.soa_record.update_serial!
+    end
 
-      20.times do
-        Factory.create(:a, :user => user, :domain => entrydns_org)
-      end
+    20.times do
+      Factory.create(:a, :user => user, :domain => entrydns_org)
     end
   end
   
