45ffdff8ef8d123865010ab4c98a50e3955969b7
Nicolae Claudius <claudius.nicolae@gmail.com>
2013-09-19 23:19:44 +0300

    oops focus

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Admin do
   include_context "data"
   
-  it "is inactive by default", focus: true do
+  it "is inactive by default" do
     admin.should_not be_active
   end
 end
