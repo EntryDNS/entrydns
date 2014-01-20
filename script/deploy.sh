c55be00c1eb252a7fcaaf16fd835a888904ab28c
Vaidas Jablonskis <jablonskis@gmail.com>
2014-01-20 20:45:53 +0000

    scripts: update deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,13 +7,12 @@ fi
 if [[ $# != 1 ]]; then
   echo 'Specify rpm file to deploy'
   exit 1
-else:
-  package=${1}
 fi
 
-yum update -y ${package}
-su - entrydns -c 'bundle exec rake db:migrate RAILS_ENV=production'
+sudo yum update -y $1
+su - entrydns -s /bin/bash -c 'bundle exec rake db:migrate RAILS_ENV=production'
 
 systemctl reload unicorn-entrydns.service
 
 systemctl status unicorn-entrydns.service
+
